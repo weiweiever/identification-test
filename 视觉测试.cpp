29439308207e17486aad3fccf9b5ca842eae44c0
weiweiever <z945429484@outlook.com>
2017-12-03 11:34:17 +0800

    new

--- 视觉测试.cpp
+++ 视觉测试.cpp
@@ -6,22 +6,21 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <iostream>
-#include "find.h"
 
 using namespace std;
 using namespace cv;
 
-Mat frame,gray;
+Mat frame, gray;
 vector<vector<Point> > contours;
 
 int main()
 {
-	VideoCapture cap("G://im//21.avi");
+	VideoCapture cap("G://im//10.avi");
 	if (!cap.isOpened())
 	{
 		cout << "无法加载此文件." << endl;
 	}
-	VideoWriter writer("G://im//201.avi", -1, 30,Size(640,480),true) ;
+	VideoWriter writer("G://im//solution2.avi", CV_FOURCC('X', 'V', 'I', 'D'), 30, Size(640, 480), true);	//CV_FOURCC('X', 'V', 'I', 'D')
 
 	while (1)
 	{
@@ -30,9 +29,10 @@ int main()
 		gray.create(frame.rows, frame.cols, CV_8UC1);
 		MatIterator_<Vec3b> it1;
 		MatIterator_<uchar> it2 = gray.begin<uchar>();
+		blur(frame, frame, Size(2, 2));
 		for (it1 = frame.begin<Vec3b>(); it1 != frame.end<Vec3b>(); it1++)		//二值化
 		{
-			if (abs((*it1)[0] - (*it1)[1])>0  || (*it1)[0] < 100 || ((*it1)[2] < (*it1)[1] ))
+			if (abs((*it1)[2] - (*it1)[1]) > 0 || (*it1)[2] < 230 || ((*it1)[0] < (*it1)[1]))
 			{
 				*it2 = 0;
 				it2++;
@@ -42,17 +42,16 @@ int main()
 			it2++;
 		}
 		imshow("gray", gray);
-		cvWaitKey(3);
+		waitKey(10);
 		vector<Vec4i> hierarchy;
 		findContours(gray, contours, hierarchy, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_NONE);		//CV_CHAIN_APPROX_SIMPLE ； CV_CHAIN_APPROX_NONE
 
-		/// 对每个找到的轮廓创建可倾斜的边界框 
+																								/// 对每个找到的轮廓创建可倾斜的边界框 
 		vector<RotatedRect> minRect;
-
+		float max_height = 0.0;
 		for (int i = 0; i < contours.size(); i++)
 		{
 			if (contours[i].size() < 4)continue;
-			float max_height = 0.0;
 			RotatedRect Rect = minAreaRect(Mat(contours[i]));
 			if (Rect.size.height < Rect.size.width)		//如果宽大于长，重新计算
 			{
@@ -65,24 +64,24 @@ int main()
 			}
 			float ratial = Rect.size.height / (float)Rect.size.width;
 
-			if (Rect.angle > -30 && Rect.angle < 20 && (ratial > 2.4) && (ratial < 8))		//筛选角度、长宽比
+			if (Rect.angle > -15 && Rect.angle < 15 && (ratial > 1.5) && (ratial < 10))		//筛选角度、长宽比
 				if ((Scalar)frame.at<Vec3b>(Rect.center) == Scalar(255, 255, 255))		//中心为纯白色
 					if (Rect.size.area() != 0.0)
-						if (Rect.size.height > (max_height / 2))		//不太小
+						if (Rect.size.height >(max_height / (float)1.8))		//不太小
 							minRect.push_back(Rect);
 		}
 		/// 绘出轮廓及其可倾斜的边界框
 		for (int i = 0; i < minRect.size(); i++)
 		{
 			// rotated rectangle  
-			Point2f rect_points[4]; 
+			Point2f rect_points[4];
 			minRect[i].points(rect_points);
 			for (int j = 0; j < 4; j++)
-				line(frame, rect_points[j], rect_points[(j + 1) % 4], Scalar(255,30,30), 2, 8);
+				line(frame, rect_points[j], rect_points[(j + 1) % 4], Scalar(255, 30, 30), 2, 8);
 		}
 
 
-		// 结果在窗体中显示  
+		// 结果在窗体中显示
 		for (int i = 0; i < minRect.size(); i++)
 		{
 			cout << "center" << minRect[i].center << endl;
@@ -92,35 +91,50 @@ int main()
 		for (int i = 0; i < 10; i++)
 			cout << endl;
 
-		Point2f target= Point2f(0, 0);
+		Point2f target = Point2f(0, 0);
+		static Point2f lastTarget = Point2f(0, 0);
+		float bestAngle = 10;
+		static char count = 0;
 
-
-		if (minRect.size()>2)
+		if (minRect.size() > 1)
 			for (int i = 0; i < minRect.size() - 1; i++)
 			{
 				for (int j = i + 1; j < minRect.size(); j++)
 				{
-					if (abs(minRect[i].angle - minRect[j].angle) < 7.5)		//角度
-						if (abs(minRect[i].center.y - minRect[j].center.y) / max(minRect[i].size.height, minRect[j].size.height) < 0.3)	//竖直距离
-							if (abs(minRect[i].size.height - minRect[j].size.height) / (minRect[i].size.height + minRect[j].size.height) < 0.3)		//高度差
+					if (abs(minRect[i].angle - minRect[j].angle) < 8.5 && abs(minRect[i].angle - minRect[j].angle) < bestAngle)	//角度
+						if (abs(minRect[i].center.y - minRect[j].center.y) / min(minRect[i].size.height, minRect[j].size.height) < 0.56)	//中心竖直距离
+							if (abs(minRect[i].size.height - minRect[j].size.height) / (minRect[i].size.height + minRect[j].size.height) < 0.25)		//高度差
 								if (abs(minRect[i].center.x - minRect[j].center.x) / minRect[i].size.height < 3.5)
-									if (abs(minRect[i].center.x - minRect[j].center.x) / minRect[i].size.height > 2)		//目标板的长宽比
-										target=Point2f((minRect[i].center.x + minRect[j].center.x) / (float)2, (minRect[i].center.y + minRect[j].center.y) / (float)2);
-										
+									if (abs(minRect[i].center.x - minRect[j].center.x) / minRect[i].size.height > 1.65)		//目标板的长宽比
+									{
+										target = lastTarget = Point2f((minRect[i].center.x + minRect[j].center.x) / (float)2, (minRect[i].center.y + minRect[j].center.y) / (float)2);
+										bestAngle = abs(minRect[i].angle - minRect[j].angle);
+									}
+									else cout << "5" << endl;
+								else cout << "4" << endl;
+							else cout << "3" << endl;
+						else cout << "2" << endl;
+					else cout << "1" << endl;
 				}
 			}
 
-
 		if (target != Point2f(0, 0))	//画出射击目标
+		{
 			circle(frame, target, 20, Scalar(30, 255, 30), 2);
-
-		imshow("show", frame);
-		cvWaitKey(5);
+			count = 0;
+		}
+		else if (target == Point2f(0, 0) && count < 1)	//画出上一个射击目标
+		{
+			circle(frame, lastTarget, 20, Scalar(30, 255, 30), 2);
+			count++;
+		}
 		writer << frame;
+		imshow("show", frame);
+		if (waitKey(5) == 27)waitKey(0);
 	}
-
 	system("pause");
-    return 0;
+	return 0;
 }
+
 
 	
